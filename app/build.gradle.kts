615b035e96663019b77c7f4b1c446e78fccd7317
sunnengsen <sunnengsen@gmail.com>
2024-10-08 15:14:15 +0700

    update sign up or register

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,12 @@ android {
     }
 }
 
-dependencies {
 
+dependencies {
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
+    // Existing dependencies
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
